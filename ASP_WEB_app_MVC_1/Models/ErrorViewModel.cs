d3cd2eb1877b0a08d22cba2add1cd527791fc1f5
DmitriyFedan <dmitry061093@mail.ru>
2023-08-22 23:10:02 +0700

    create asp.net mvc project

---
+++ ASP_WEB_app_MVC_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_WEB_app_MVC_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
